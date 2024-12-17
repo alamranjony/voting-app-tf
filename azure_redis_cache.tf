5d2d9618151b7b787fbeb12de7d78f92678a5700
alamranjony <alimranjohny@gmail.com>
2024-12-18 01:43:21 +0600

    import redis cache

--- azure_redis_cache.tf
+++ azure_redis_cache.tf
@@ -1,13 +1,17 @@
 resource "azurerm_redis_cache" "main" {
-  name                 = "votingapp-cache"
-  location             = azurerm_resource_group.main.location
-  resource_group_name  = azurerm_resource_group.main.name
-  capacity             = 0
-  family               = "C"
-  sku_name             = "Standard"
-  non_ssl_port_enabled = false
-  minimum_tls_version  = "1.2"
+  name                               = "votingapp-001"
+  location                           = azurerm_resource_group.main.location
+  resource_group_name                = azurerm_resource_group.main.name
+  capacity                           = 0
+  family                             = "C"
+  sku_name                           = "Standard"
+  non_ssl_port_enabled               = false
+  minimum_tls_version                = "1.2"
+  access_keys_authentication_enabled = false
+  public_network_access_enabled      = false
 
   redis_configuration {
+    active_directory_authentication_enabled = true
+    maxmemory_policy                        = "volatile-lru"
   }
 }
